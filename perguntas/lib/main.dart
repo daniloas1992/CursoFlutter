77fb87a4132aaf8c7984ed4d07a0b97f089e5e47
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-01-10 22:12:22 -0300

    Stateless Widget

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -4,14 +4,44 @@ main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
 
+  void responder() {
+    print('Pergunta respondida #1!');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var perguntaSelecionada = 0;
+
+    final List<String> perguntas = [
+      'Qual sua cor favorita?',
+      'Qual seu animal favorito?'
+    ];
+
     return MaterialApp(
+
       home: Scaffold(
+
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
       ),
     );
   }
