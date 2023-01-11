972d24a54bee453c106d8654a4a622b330be6a99
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-01-10 22:25:50 -0300

    Usando StatefulWidget #02

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -2,47 +2,62 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
-
-  void responder() {
-    print('Pergunta respondida #1!');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
+class PerguntaAppState extends State<PerguntaApp> {
+    
     var perguntaSelecionada = 0;
 
-    final List<String> perguntas = [
-      'Qual sua cor favorita?',
-      'Qual seu animal favorito?'
-    ];
+    void responder() {
+      
+      setState(() {
+        perguntaSelecionada++;
+      });
 
-    return MaterialApp(
+      print(perguntaSelecionada);
+    }
 
-      home: Scaffold(
+    @override
+    Widget build(BuildContext context) {
 
-        appBar: AppBar(
-          title: Text('Perguntas'),
+
+      final List<String> perguntas = [
+        'Qual sua cor favorita?',
+        'Qual seu animal favorito?'
+      ];
+
+      return MaterialApp(
+
+        home: Scaffold(
+
+          appBar: AppBar(
+            title: Text('Perguntas'),
+          ),
+          body: Column(
+            children: [
+              Text(perguntas[perguntaSelecionada]),
+              ElevatedButton(
+                child: Text('Resposta 1'),
+                onPressed: responder,
+              ),
+              ElevatedButton(
+                child: Text('Resposta 2'),
+                onPressed: responder,
+              ),
+              ElevatedButton(
+                child: Text('Resposta 3'),
+                onPressed: responder,
+              ),
+            ],
+          ),
         ),
-        body: Column(
-          children: [
-            Text(perguntas[perguntaSelecionada]),
-            ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: responder,
-            ),
-          ],
-        ),
-      ),
-    );
+      );
+    }
+}
+
+class PerguntaApp extends StatefulWidget {
+
+  @override
+  PerguntaAppState createState() {
+    return new PerguntaAppState();
   }
+
 }
