93a2b9cf47fd9e79012c2fd5251549325afdb5a2
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-01-10 22:32:26 -0300

    Tornando Membros Privados

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaAppState extends State<PerguntaApp> {
+class _PerguntaAppState extends State<PerguntaApp> {
     
-    var perguntaSelecionada = 0;
+    var _perguntaSelecionada = 0;
 
-    void responder() {
+    void _responder() {
       
       setState(() {
-        perguntaSelecionada++;
+        _perguntaSelecionada++;
       });
 
-      print(perguntaSelecionada);
+      print(_perguntaSelecionada);
     }
 
     @override
@@ -33,18 +33,18 @@ class PerguntaAppState extends State<PerguntaApp> {
           ),
           body: Column(
             children: [
-              Text(perguntas[perguntaSelecionada]),
+              Text(perguntas[_perguntaSelecionada]),
               ElevatedButton(
                 child: Text('Resposta 1'),
-                onPressed: responder,
+                onPressed: _responder,
               ),
               ElevatedButton(
                 child: Text('Resposta 2'),
-                onPressed: responder,
+                onPressed: _responder,
               ),
               ElevatedButton(
                 child: Text('Resposta 3'),
-                onPressed: responder,
+                onPressed: _responder,
               ),
             ],
           ),
@@ -56,8 +56,8 @@ class PerguntaAppState extends State<PerguntaApp> {
 class PerguntaApp extends StatefulWidget {
 
   @override
-  PerguntaAppState createState() {
-    return new PerguntaAppState();
+  _PerguntaAppState createState() {
+    return new _PerguntaAppState();
   }
 
 }
