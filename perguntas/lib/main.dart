a5d366d0cb0e82b4b0afacd814724c0bf96e9e79
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-01-16 22:08:53 -0300

    Widget Questionário

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
 import './resultado.dart';
+import './questionario.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -38,7 +37,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
     @override
     Widget build(BuildContext context) {
 
-      List<String> respostas = temPerguntaSelecionada ? _perguntas[_perguntaSelecionada].cast()['respostas'] : [];
+      
 
       return MaterialApp(
 
@@ -47,12 +46,11 @@ class _PerguntaAppState extends State<PerguntaApp> {
           appBar: AppBar(
             title: Text('Perguntas'),
           ),
-          body: temPerguntaSelecionada ? Column(
-            children: <Widget>[
-              Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
-              ...respostas.map((t) => Resposta(t, _responder)).toList(),
-            ],
-          ) : Resultado(),
+          body: temPerguntaSelecionada 
+            ? Questionario(perguntas: _perguntas, 
+                           perguntaSelecionada: _perguntaSelecionada, 
+                           quandoResponder: _responder) 
+            : Resultado(),
         ),
       );
     }
