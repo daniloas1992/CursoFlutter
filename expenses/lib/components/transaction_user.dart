b726e98fca20599091021a737de0d1ea86ff5dd1
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-01-31 22:46:51 -0300

    Adicionando Transação #01

--- expenses/lib/components/transaction_user.dart
+++ expenses/lib/components/transaction_user.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'transaction_form.dart';
 import 'transaction_list.dart';
 import '../models/transaction.dart';
+import 'dart:math';
 
 class TransactionUser extends StatefulWidget {
   const TransactionUser({super.key});
@@ -16,6 +17,19 @@ class _TransactionUserState extends State<TransactionUser> {
     Transaction(id: 't1', title: 'Novo tênis', value: 310.25, date: DateTime.now()),
     Transaction(id: 't2', title: 'Conta Luz', value: 115.87, date: DateTime.now())
   ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
