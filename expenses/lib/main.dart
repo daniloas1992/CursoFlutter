0451c0522588e8e7c4aa1e4fe23db4055aa6b3e5
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-03-26 20:55:19 -0300

    Fechando Formulário (Modal)

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -47,6 +47,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop(); //Fecha o form 
   }
 
 
